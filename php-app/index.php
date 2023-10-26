80da7171b34d830c27345c872889422b1483e744
akshai <akshaiakshai35@gmail.com>
2023-10-26 15:01:38 +0530

    feat: modified index.php

--- php-app/index.php
+++ php-app/index.php
@@ -4,11 +4,12 @@ $pass = rtrim(file_get_contents("/run/secrets/secret"));
 
 $conn = new mysqli("db", "root", $pass);
 
-if ($conn->connect_error) {
-    echo "Unable to connect";
-} else {
-    echo "Connected to PHP successfully"; // Added a semicolon at the end of this line
-}
+echo "Hello from php"
+//if ($conn->connect_error) {
+ //   echo "Unable to connect";
+//} else {
+ //   echo "Connected to PHP successfully"; // Added a semicolon at the end of this line
+//}
 
 ?>
 
