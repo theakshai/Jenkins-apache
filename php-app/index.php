ab5217e6bc04912f9ad56c963a180bd6c59d8881
akshai <akshaiakshai35@gmail.com>
2023-10-26 12:47:33 +0530

    feat:Modified php file

--- php-app/index.php
+++ php-app/index.php
@@ -4,14 +4,11 @@ $pass = rtrim(file_get_contents("/run/secrets/secret"));
 
 $conn = new mysqli("db", "root", $pass);
 
-if($conn->connect_error) {
-
+if ($conn->connect_error) {
     echo "Unable to connect";
-
 } else {
-
-    echo "Connected to php successfully"
-
+    echo "Connected to PHP successfully"; 
 }
 
 ?>
+
